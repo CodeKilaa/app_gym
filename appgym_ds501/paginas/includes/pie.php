ea79e14de8a195046704f46629a2de498ea8ca8e
Codekilaa <xluixarturox123@gmail.com>
2024-08-22 09:57:09 -0500

    subiendo al repositorio

---
+++ appgym_ds501/paginas/includes/pie.php
@@ -0,0 +1,7 @@
+<footer class="mt-3">
+    <div class="alert alert-primary text-center" role="alert">
+        <h5>Bienvenido(a) <?=$_SESSION["cliente"]->nombre?> </h5>
+        <span> &copy; App GYM - Desarrollado por Luis Velasquez</span>
+    </div>
+</footer>
+</body>
